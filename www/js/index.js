ab45eff8f884f92f53cb873db61d038261106c08
Jean-Christophe Hoelt <hoelt@fovea.cc>
2014-10-17 12:08:57 +0300

    Documenting the code

--- www/js/index.js
+++ www/js/index.js
@@ -1,13 +1,15 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Copyright (c) 2014, Jean-Christophe Hoelt <hoelt@fovea.cc>
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -17,119 +19,181 @@
  * under the License.
  */
 
-function log(arg) { app.log(arg); }
+// Our application's global object
+var app = {};
 
-var app = {
+//
+// Constructor
+// -----------
+//
 
-    log: function(arg) {
-        try {
-            if (typeof arg !== 'string')
-                arg = JSON.stringify(arg);
-            console.log(arg);
-            document.getElementById('log').innerHTML += '<div>' + arg + '</div>';
-        } catch (e) {}
-    },
+app.initialize = function() {
+    log('initialize');
 
-    // Application Constructor
-    initialize: function() {
-        log('initialize');
-        this.bindEvents();
-    },
+    // Listen to the deviceready event.
+    // Make sure the score of 'this' isn't lost.
+    document.addEventListener('deviceready', this.bind(this.onDeviceReady), false);
+};
 
-    // Bind Event Listeners
+//
+// Methods
+// -------
+//
+
+// deviceready event handler.
+//
+// will just initialize the Purchase plugin
+app.onDeviceReady = function() {
+    log('onDeviceReady');
+    this.initStore();
+};
+
+// initialize the purchase plugin if available
+app.initStore = function() {
+
+    if (!window.store) {
+        log('Store not available');
+        return;
+    }
+
+    // Enable maximum logging level
+    store.verbosity = store.DEBUG;
+
+    // Inform the store of your products
+    log('registerProducts');
+    store.registerProducts([{
+        id:    'cc.fovea.purchase.consumable1',
+        alias: 'extra life',
+        type:   store.CONSUMABLE
+    }, {
+        id:    'cc.fovea.purchase.nonconsumable1',
+        alias: 'full version',
+        type:   store.NON_CONSUMABLE
+    }]);
+
+    // When any product gets updated, refresh the HTML.
+    store.when("product").updated(function (p) {
+        app.renderIAP(p);
+    });
+
+    // Log all errors
+    store.error(function(error) {
+        log('ERROR ' + error.code + ': ' + error.message);
+    });
+
+    // When purchase of an extra life is approved,
+    // deliver it... by displaying logs in the console.
+    store.when("extra life").approved(function (order) {
+        log("You got an EXTRA LIFE!");
+        order.finish();
+    });
+
+    // When purchase of the full version is approved,
+    // show some logs and finish the transaction.
+    store.when("full version").approved(function (order) {
+        log('You just unlocked the FULL VERSION!');
+        order.finish();
+    });
+
+    // The play button can only be accessed when the user
+    // owns the full version.
+    store.when("full version").updated(function (product) {
+        document.getElementById("play-button").style.display =
+            (product.state === store.OWNED) ? "block" : "none";
+    });
+
+    // When the store is ready (i.e. all products are loaded and in their "final"
+    // state), we hide the "loading" indicator.
+    // 
+    // Note that the "ready" function will be called immediately if the store
+    // is already ready.
+    store.ready(function() {
+        document.getElementById("loading-indicator").style.display = 'none';
+    });
+
+    // Alternatively, it's technically feasible to have a button that
+    // is always visible, but shows an alert if the full version isn't
+    // owned.
+    // ... but your app may be rejected by Apple if you do it this way.
     //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        log('bindEvents');
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
- 
-    // deviceready Event Handler
+    // Here is the pseudo code for illustration purpose.
+
+    // myButton.onclick = function() {
+    //   store.ready(function() {
+    //     if (store.get("full version").owned) {
+    //       // access the awesome feature
+    //     }
+    //     else {
+    //       // display an alert
+    //     }
+    //   });
+    // };
+
+
+    // Refresh the store.
     //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        log('onDeviceReady');
-        app.initializeIAP();
-    },
+    // This will contact the server to check all registered products
+    // validity and ownership status.
+    //
+    // It's fine to do this only at application startup, as it could be
+    // pretty expensive.
+    log('refresh');
+    store.refresh();
+};
 
-    initializeIAP: function() {
-        if (!window.store) {
-            log('Store not available');
-            return;
+app.renderIAP = function(p) {
+
+    var elId = p.id.split(".")[3];
+
+    var el = document.getElementById(elId + '-purchase');
+    if (!el) return;
+
+    if (!p.loaded) {
+        el.innerHTML = '<h3>...</h3>';
+    }
+    else if (!p.valid) {
+        el.innerHTML = '<h3>' + p.alias + ' Invalid</h3>';
+    }
+    else if (p.valid) {
+        var buttonStyle = "";
+        var html = "<h3>" + p.title + "</h3>" + "<p>" + p.description + "</p>";
+        if (p.canPurchase) {
+            html += "<div style='" + buttonStyle + "' id='buy-" + p.id + "' productId='" + p.id + "' type='button'>" + p.price + "</div>";
         }
-
-        store.debug = store.DEBUG;
-
-        log('registerProducts');
-        store.registerProducts([{
-            id:    'cc.fovea.purchase.consumable1',
-            alias: 'extra life',
-            type:   store.CONSUMABLE
-        }, {
-            id:    'cc.fovea.purchase.nonconsumable1',
-            alias: 'full version',
-            type:   store.NON_CONSUMABLE
-        }]);
-
-        store.ask("full version").then(function (p) {
-            log("full version loaded");
-        }).error(function (err, p) {
-            log("full version loaded with error");
-        });
-
-        // When any product gets updated, refresh the HTML.
-        store.when("product").updated(function (p) {
-            app.renderIAP(p);
-        });
-
-        store.error(function(error) {
-            log('ERROR ' + error.code + ': ' + error.message);
-        });
-
-        store.when("extra life").approved(function (order) {
-            log("EXTRA LIFE!");
-            order.finish();
-        });
-
-        store.when("full version").approved(function (order) {
-            log('Unlocking FULL VERSION!');
-            order.finish();
-        });
-
-        log('refresh');
-        store.refresh();
-    },
-
-    renderIAP: function(p) {
-
-        var elId = p.id.split(".")[3];
-
-        var el = document.getElementById(elId + '-purchase');
-        if (!el) return;
-
-        if (!p.loaded) {
-            el.innerHTML = '<h3>...</h3>';
-        }
-        else if (!p.valid) {
-            el.innerHTML = '<h3>' + p.alias + ' Invalid</h3>';
-        }
-        else if (p.valid) {
-            var buttonStyle = "display:inline-block; padding: 5px 20px; border: 1px solid black";
-            var html = "<h3>" + p.title + "</h3>" + "<p>" + p.description + "</p>";
-            if (p.canPurchase) {
-                html += "<div style='" + buttonStyle + "' id='buy-" + p.id + "' productId='" + p.id + "' type='button'>" + p.price + "</div>";
-            }
-            el.innerHTML = html;
-            if (p.canPurchase) {
-                document.getElementById("buy-" + p.id).onclick = function (event) {
-                    var pid = this.getAttribute("productId");
-                    store.order(pid);
-                };
-            }
+        el.innerHTML = html;
+        if (p.canPurchase) {
+            document.getElementById("buy-" + p.id).onclick = function (event) {
+                var pid = this.getAttribute("productId");
+                store.order(pid);
+            };
         }
     }
 };
+
+// 
+// Utilities
+// ---------
+//
+
+// shortcut to the app logging function.
+function log(arg) { app.log(arg); }
+
+// log both in the console and in the HTML #log element.
+app.log = function(arg) {
+    try {
+        if (typeof arg !== 'string')
+            arg = JSON.stringify(arg);
+        console.log(arg);
+        document.getElementById('log').innerHTML += '<div>' + arg + '</div>';
+    } catch (e) {}
+};
+
+// make sure fn will be called with app as 'this'
+app.bind = function(fn) {
+    return function() {
+        fn.call(app, arguments);
+    };
+};
+
 
 app.initialize();
